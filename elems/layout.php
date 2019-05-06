e1f4d230f18a909fc036af8249a960c816e320ca
Turuntuntun <43621680+Turuntuntun@users.noreply.github.com>
2019-05-06 18:35:02 +0300

    Перемещение верхнего меню

--- elems/layout.php
+++ elems/layout.php
@@ -15,45 +15,7 @@
 	<div class = "logo">
 	</div>
  	<header>
-	 	<div class = "wrapper">
-	 		<div class = 'href1'>
-		 			<a href = "#">
-		 				<img src = '../css/images/Lemon_Logo.png' id = 'smal_logo'>
-		 			</a>	    
-	 		</div>
-	 		<nav>
-	 			<ul class = "nav">
-	 				<li class = "menu_item" id ='first-child'>
-                        <a href="">HOME</a>
-                    </li>
-	 				<li class = "menu_item">
-                        <a href="">RECIPES</a>
-		 				<ul>
-		 					<li class = "menu_item"><a href="">RECOMENDED</a></li>
-		 					<li class = "menu_item"><a href="">POPULAR</a></li>
-		 					<li class = "menu_item"><a href="">QUICK & EASY</a></li>
-		 					<li class = "menu_item"><a href="">HEALTHY</a></li>
-		 					<li class = "menu_item"><a href="">NEWEST</a></li>
-		 				</ul>
-	 				</li>
-	 				<li class = "menu_item" ><a href="">PHOTO GALERIES</a>
-						<ul id = 'li_li'>
-		 					<li class = "menu_item"><a href="">NEW</a></li>
-		 					<li class = "menu_item"><a href="">TOP</a></li>
-		 					<li class = "menu_item"><a href="">RATEST</a></li>
-		 				</ul>
-	 				</li>
-	 				<li class = "menu_item">
-                        <a href="">VIDEOS</a>
-                    </li>
-	 				<li class = "menu_item">
-                        <a href="">ALL CATEGORIES</a>
-                    </li>
-	 		    </ul>
-	 		</nav>
-
-	 		<input type = "text"  placeholder = "FIND A RECIPE" id = 'find_recipe'>
-	    </div>
+        <? include 'elems/header.php'; ?>
 	</header>
 	<main>
 		<?php
