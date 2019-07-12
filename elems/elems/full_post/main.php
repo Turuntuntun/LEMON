c0a5809a84fff4e21ae2ab76e2cf591f972aeca9
Turuntuntun <43621680+Turuntuntun@users.noreply.github.com>
2019-07-12 22:52:02 +0300

    Настроен html + css блока полный пост

--- elems/elems/full_post/main.php
+++ elems/elems/full_post/main.php
@@ -1,21 +1,21 @@
 <div class = "full_post">
-    <div class = "photo_icon">
+    <div class = "icon">
     </div>
-    <div class = "recipe_text">
-        <span class = "recipe_zag">Bananas Foster Ice Cream Cake </span>
-        <div class = "recipe_smal_zag">
-            <div class = "icon_time"></div>
-            <span class = "mins" id = 'rec_mins'>15mins</span>
-            <div class = "icon_easy"></div>
-            <span class = "mins" id = "easy">Easy</span>
+    <div class = "block">
+        <span class = "title">Bananas Foster Ice Cream Cake </span>
+        <div class = "small">
+            <div class = "time"></div>
+            <span class = "text"'>15mins</span>
+            <div class = "easy"></div>
+            <span class = "text">Easy</span>
             <input type = "button" value = "SAVE RECIPE">
         </div>
-        <div class = "recipe_recipe">
+        <div class = "recipe">
             <div class = "ingridients">
                 <p>Яйца - 4 шт. Мука - 120 г Сахар - 120 г + сахар для крема Сахар ванильный - 1 ч.л. Разрыхлитель - 1 ч.л. Какао-порошок - 1,5 ст.л. Бананы крупные - 3 шт. Сметана (20% жирности и более) - 1 большая банка</p>
             </div>
             <span class = 'direct'>Directions</span>
-            <div class = 'full_recipe'>
+            <div class = 'full'>
                 ллафл афлафл афылаы фдаы фдафдадф ыдафо аыфлалф ыалдфы фыл аолыфа лфадфы флыадыфд афлдад фылаф  дфлыаф оыал фоыд дфафы адфыоа фа лдфыа лдфы афоал фыалофы ыда офл алдфыола офыо аыфлоалыф фыл олфд алыд фаодф ыаофыоа ало фыло алдфо ыалфал .
                 Fjgjjdd jjdjadja sjabdjasb js jadjas jasjfbjas jsadasjj jasfjajas jjsabdjajas jsabdj asdjsa jsbddasj sdbs.
                 djdjdjdj djdjdjdjdjdjdjdjjda lslaksaksaksaksa.
