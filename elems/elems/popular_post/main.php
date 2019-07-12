ccfd083d59c1e147f07ed0ff738279388ec1c53b
Turuntuntun <43621680+Turuntuntun@users.noreply.github.com>
2019-07-12 22:37:34 +0300

    Настроен html + css блока популярный пост

---
+++ elems/elems/popular_post/main.php
@@ -0,0 +1,25 @@
+<div class = 'popular'>
+    <div class = 'title'>
+        <p>Popular Now</p>
+    </div>
+    <hr class = 'hr'>
+    <div>
+        <div class = 'headline'>
+            <p>Vanila Frozen Coffee</p>
+        </div>
+        <div class = 'text'>
+            <p>Mix coffee, sugar and creamer. <br> Pour into blender and add ice cubes. <br> Blend until smooth.</p>
+        </div>
+    </div>
+    <div class = 'block'>
+        <input type = 'button' value = 'GET IT RECIPE'>
+        <div class = "under">
+            <div class = "time"></div>
+            <span>6mins |</span>
+            <div class = "comment"></div>
+            <span>240 </span>
+            <div class = "viem"></div>
+            <span>400</span>
+        </div>
+    </div>
+</div>
