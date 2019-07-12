ff0a74807146aa0e93d9eff0c6fb807ebcedb3f8
Turuntuntun <43621680+Turuntuntun@users.noreply.github.com>
2019-07-12 22:55:59 +0300

    Настроен html + css блока время рецептов

---
+++ elems/elems/time_of_recipe/main.php
@@ -0,0 +1,43 @@
+<div class = 'timeline'>
+    <div class = 'void'>
+    </div>
+    <div class = 'title'>
+        <p>Timeline</p>
+        <div class = 'icon'>
+        </div>
+    </div>
+    <hr class = 'hr'>
+    <div class = 'headline'>
+        <p>Time line</p>
+    </div>
+    <div class = 'block'>
+        <div class = 'subBlock'>
+            <p class = 'time'>EASY</p>
+            <div class = 'small'>
+                <p>15mins</p>
+                <div class = 'easy'>
+                </div>
+            </div>
+        </div>
+        <div class = 'subBlock'>
+            <p class = 'time'>MIDDLE</p>
+            <div class = 'small'>
+                <p>25mins</p>
+                <div class = 'middle'>
+                </div>
+            </div>
+        </div>
+        <div class = 'subBlock'>
+            <p class = 'time'>LONG</p>
+            <div class = 'small'>
+                <p>40mins</p>
+                <div class = 'long'>
+                </div>
+            </div>
+        </div>
+    </div>
+    <hr class = 'smallHr'>
+    <div class = 'under'>
+        <p>Eam adhuc iudico percipitur ex, ut malis audire voluptaria has. </p>
+    </div>
+</div>
