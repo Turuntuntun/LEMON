2b410d1ff8d1abf8723af53f863d0bc7f5a14ae7
Turuntuntun <43621680+Turuntuntun@users.noreply.github.com>
2019-07-12 23:22:02 +0300

    Настроен html + css блока ???

---
+++ elems/elems/target/main.php
@@ -0,0 +1,46 @@
+<div class = 'target'>
+    <div class = 'title'>
+        <p>Target</p>
+        <div class = 'icon'>
+        </div>
+    </div>
+    <hr class = 'hr'>
+    <div class = 'headline'>
+        <p>Presentation line</p>
+    </div>
+    <div>
+        <div class = 'block' id = 'first'>
+            <div class = 'background'>
+                <div class = 'border'>
+                    <p>19%</p>
+                </div>
+            </div>
+            <div class = 'subBlock'>
+                <p class = 'title'>Housewifes</p>
+                <p class = 'text'>Lorem ipsum dolor sit amet, prima ceteros.</p>
+            </div>
+        </div>
+        <div class = 'block' id = 'second'>
+            <div class = 'background'>
+                <div class = 'border'>
+                    <p>65%</p>
+                </div>
+            </div>
+            <div class = 'subBlock'>
+                <p class = 'title'>Newlyweds</p>
+                <p class = 'text'>Has brute elitr deserunt ea, assentior eloquen- tiam neglegentur te sed.</p>
+            </div>
+        </div>
+        <div class = 'block' id = 'third'>
+            <div class = 'background'>
+                <div class = 'border'>
+                    <p>50%</p>
+                </div>
+            </div>
+            <div class = 'subBlock'>
+                <p class = 'title'>Business lady</p>
+                <p class = 'text'>Ei nam primis putent signiferumque.</p>
+            </div>
+        </div>
+    </div>
+</div>
