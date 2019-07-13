e27c32bdcbef3673a8c42e5daa15d70e070b876c
Turuntuntun <43621680+Turuntuntun@users.noreply.github.com>
2019-07-13 21:22:00 +0300

    Настроен html + css блока сортировка рецептов

---
+++ elems/elems/posts_for_user/main.php
@@ -0,0 +1,37 @@
+<div class = 'postsForUser'>
+    <div class = 'title'>
+        <p>Recipe</p>
+        <div class = 'icon'>
+        </div>
+    </div>
+    <hr class = 'hr'>
+    <div class = 'type'>
+        <div>Easy</div>
+        <div>Middle</div>
+        <div>Long</div>
+    </div>
+    <div class = 'block'>
+        <div class = 'recipe'>
+            <img class = 'img' src="../elems/elems/posts_for_user/images/Photo_rec_side1.png">
+            <div class = 'text'>
+                <p>by <span class = 'bold'>Smuckersreg Toppings</span></p>
+                <p><span class = 'big'>Thanks for the recipe lorem ipsum bla bla bla Thanks for the recipe lore</span></p>
+            </div>
+        </div>
+        <div class = 'recipe'>
+            <img class = 'img' src="../elems/elems/posts_for_user/images/Photo_rec_side2.png">
+            <div class = 'text'>
+                <p>by <span class = 'bold'>Smuckersreg Toppings</span></p>
+                <p><span class ="big">Thanks for the recipe lorem nks for the recipe</span></p>
+            </div>
+        </div>
+        <div class = 'recipe'>
+            <img class = 'img' src="../elems/elems/posts_for_user/images/Photo_rec_side3.png">
+            <div class = 'text'>
+                <p>by <span class = 'bold'>Smuckersreg Toppings</span></p>
+                <p><span class = 'big'>Thanks for the recipe lorem nks for the recipe</span></p>
+            </div>
+        </div>
+    </div>
+    <input type = 'button' value = 'VIEM MORE' class = 'viem_more'>
+</div>
