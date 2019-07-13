4fcb352aca9520f0f651414141bde110cc1ed474
Turuntuntun <43621680+Turuntuntun@users.noreply.github.com>
2019-07-13 13:37:13 +0300

    Настроен html + css блока поиск

---
+++ elems/elems/search/main.php
@@ -0,0 +1,6 @@
+<form action ='' method='GET'>
+    <div class="search">
+        <input type = 'submit' value = ' '>
+        <input type = "search"  placeholder = "FIND A RECIPE"'>
+    </div>
+</form>
