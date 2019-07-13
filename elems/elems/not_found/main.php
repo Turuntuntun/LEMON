88e26ae896a66d12a539c012d86b0bb859814909
Turuntuntun <43621680+Turuntuntun@users.noreply.github.com>
2019-07-13 16:22:47 +0300

    Настроен html + css блока не найдено

---
+++ elems/elems/not_found/main.php
@@ -0,0 +1,16 @@
+<div class = 'not_found'>
+    <div class = "title">
+        <span>404</span>
+        <hr class = 'hr'>
+    </div>
+    <div class = 'block'>
+        <p class = 'title'>Sorry</p>
+        <p class = "headline">WE COUlNDT </p>
+        <p class = "text">FIND THE PAGE</p>
+        <div class = 'icon'>
+        </div>
+    </div>
+    <div class = "back">
+        <input type = 'button' value = 'BACK'>
+    </div>
+</div>
