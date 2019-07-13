16b53b44ecb941e4324b3f044dd518e71b8bbc2c
Turuntuntun <43621680+Turuntuntun@users.noreply.github.com>
2019-07-13 21:47:23 +0300

    Настроен html + css блока про сайт

---
+++ elems/elems/elem_about_site/main.php
@@ -0,0 +1,23 @@
+<div class = 'autor'>
+    <div class = "title">
+        Autor
+        <hr class = 'hr'>
+    </div>
+    <div class = 'block'>
+        <div class = 'photo'>
+        </div>
+        <div class = 'subBlock'>
+            <p class = 'name'>Karen Gray</p>
+            <p class = 'country'>United States</p>
+            <div class = 'icon'>
+            </div>
+            <hr class = 'smallHr'>
+            <div class = 'about'>
+                <p class = 'title'>About Lemon</p>
+                <p class = 'text'>Over the fifteen-year life span of Food.com, we’ve learned that – in addition to eating – sharing is what you do best. And thanks to the 20 million of you who come here each month, we now have 500,000 recipes to show for it, more than anywhere else in the digital universe. We also have tons crazy-tempting photos, troves of recipe reviews and more than 2 million Facebook likes. That’s a heck of a lot of Food. Thank you!</p>
+            </div>
+            <div class = 'autrograph'>
+            </div>
+        </div>
+    </div>
+</div>
