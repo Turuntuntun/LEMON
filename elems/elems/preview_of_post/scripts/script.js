b0a7674be9f48c62883388c0fb2d38a52a130ad2
Turuntuntun <43621680+Turuntuntun@users.noreply.github.com>
2019-07-31 00:07:36 +0300

    Добавление кнопок теперь в js

--- elems/elems/preview_of_post/scripts/script.js
+++ elems/elems/preview_of_post/scripts/script.js
@@ -1,26 +1,63 @@
-function slider(images,delay){
-    let k = 1;
+function slider(images, delay) {
 
-    $('.preview .radio input').on('click', function () {
-        k = $(this).val();
-        changeImage($(this).val());
-    });
-    $('#preview0').trigger('click');
+    context = this;
+    this.k = 0;
+    this.radio = '.preview .radio input';
+    this.images = images;
+    this.delay = delay;
+    this.count = images.length;
+    this.event = 'click';
+    this.block = '.preview';
 
-    setInterval(function () {
-        count = images.length;
-        k++;
-        if (k === (count)) {
-            k = 0;
+    this.main = function() {
+        addRadio();
+        checkRadio();
+        console.log(context.k);
+        $('#preview0').trigger(context.event);
+
+        if(context.count > 1){
+            context.timeid = setTimeout(function run() {
+                gettingK();
+                setTimeout(run, delay);
+            }, delay);
+        }else{
+            changeImage(0);
         }
-        $('#preview' + k).trigger('click');
-    }, delay);
-    function changeImage(value) {
-        $('.preview').animate({'opacity':'0'},300,function(){
-            $(this).find('.img').attr('src',images[value]['path']);
-            $(this).find('a').text(images[value]['title']);
-            $(this).find('.text').text(images[value]['prewiew_text']);
-            $(this).animate({'opacity':'1'},300);
+    };
+
+    function gettingK() {
+        context.k++;
+        if (context.k === (context.count)) {
+            context.k = 0;
+        }
+
+        $('#preview' + context.k).trigger(context.event);
+    }
+
+    function checkRadio(){
+        $(context.radio).on(context.event, function () {
+            context.k = $(this).val();
+            changeImage($(this).val());
+            clearTimeout(context.timeid);
         });
     }
+
+    function changeImage(value) {
+        $('.preview').animate({'opacity': '0'}, 500, function () {
+            $(this).find('.img').attr('src', context.images[value]['path']);
+            $(this).find('a').text( context.images[value]['title']);
+            $(this).find('.text').text(context.images[value]['prewiew_text']);
+            $(this).animate({'opacity': '1'}, 500);
+        });
+    }
+    function addRadio() {
+        result = '';
+        for(i = 0; i < context.count; i++){
+            result += '<input  type = "radio" name = "slide" id = "preview' + i + '" value = ' + i + '>';
+            result += '<label for = "preview' + i + '"><div></div></label>';
+        }
+        $('.preview .radio').html(result);
+
+    }
 }
+
